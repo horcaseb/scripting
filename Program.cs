3ef42024ec67fba2949cc2239b3e657211541c54
horcaseb <46980001+horcaseb@users.noreply.github.com>
2019-01-24 22:55:56 -0500

    presentacion02/diapositiva14

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace triangulo_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("ingrese el valor de Y");
+            double Y = double.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese el valor de Z");
+            double Z = double.Parse(Console.ReadLine());
+            double T = Math.Sqrt((Y * Y) + (Z * Z));
+            double C1 = Math.Asin(Z/T);
+            double C = C1 * (180 / Math.PI);
+            double A1 = Math.Asin(Y / T);
+            double A = A1 * (180 / Math.PI);
+
+            Console.WriteLine(" el valor de C : " + C);
+            Console.WriteLine(" el valor de T : " + T);
+            Console.WriteLine(" el valor de A : " + A);
+        }
+    }
+}
